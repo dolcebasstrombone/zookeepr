8a2b3bc304551b90dc03e55801238b49fd7057f7
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-06-21 16:55:07 -0400

    push to hub

--- server.js
+++ server.js
@@ -1,45 +1,42 @@
-const express = require("express");
+const express = require('express');
+const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
-const app = express(); //declared so we can chain on express methods
-const { animals } = require("./data/animals");
+const app = express();
 
 function filterByQuery(query, animalsArray) {
-  let personailityTraitsArray = [];
+  let personalityTraitsArray = [];
   let filteredResults = animalsArray;
-  //personailtyTraits handled differently due to multiple traits per animal
-  if (query.personaliyTraits) {
-    if (typeof query.personaliyTraits === "string") {
-      personailityTraitsArray = [query.personaliyTraits];
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
     } else {
-      personailityTraitsArray = query.personailityTraits;
+      personalityTraitsArray = query.personalityTraits;
     }
-    personailityTraitsArray.forEach((trait) => {
+    personalityTraitsArray.forEach(trait => {
       filteredResults = filteredResults.filter(
-        (animal) => animal.personailityTraits.indexOf(trait) !== -1
+        animal => animal.personalityTraits.indexOf(trait) !== -1
       );
     });
   }
-  //diet, species, and name handling
   if (query.diet) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.diet === query.diet
-    );
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
   }
   if (query.species) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.species === query.species
-    );
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
   }
   if (query.name) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.name === query.name
-    );
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
   }
   return filteredResults;
 }
 
-//get method, 2 arguments, the route the client uses to fetch and callback function
-app.get("/api/animals", (req, res) => {
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
+app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -47,7 +44,15 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-//listen method,2 arguments, port # and callback function
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
